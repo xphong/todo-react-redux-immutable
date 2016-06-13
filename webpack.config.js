420f5b23a513f744c10ce54f2658d013ea585f44
Phong Huynh <phuynh@architech.ca>
2016-06-13 10:47:14 -0400

    Create index and app

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-  entry: '',
+  entry: './src/app.js',
   output: {
     path: './build/',
+    publicPath: 'http://localhost:8080/',
     filename: "bundle.js"
   },
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
           presets: ['react', 'es2015']
         }
       }
     ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
   }
 }
