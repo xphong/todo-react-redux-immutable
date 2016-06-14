7cd7effa9f604fb749ff70a9e3b7aeff25f05885
Phong Huynh <phuynh@architech.ca>
2016-06-14 12:31:42 -0400

    Add delete todo to reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -7,7 +7,9 @@ const INITIAL_STATE = Immutable.List(['Learn Redux']);
 export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case ADD_TODO:
-      return state.push(action.todo);
+      return state.unshift(action.todo);
+    case DELETE_TODO:
+      return state.filter((todo, index) => index !== action.index);
     default:
       return state;
   }
