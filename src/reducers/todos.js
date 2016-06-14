d05089bbc23f5142b05147add6097605451714df
Phong Huynh <phuynh@architech.ca>
2016-06-14 11:44:50 -0400

    Update reducer, create store

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,8 +1,12 @@
 import Immutable from 'immutable'
 
-export default (state = Immutable.List(['Learn Redux']), action) => {
+import { ADD_TODO, DELETE_TODO } from '../constants';
+
+const INITIAL_STATE = Immutable.List(['Learn Redux']);
+
+export default (state = INITIAL_STATE, action) => {
   switch(action.type) {
-    case 'addTodo':
+    case ADD_TODO:
       return state.push(action.todo)
     default:
       return state
